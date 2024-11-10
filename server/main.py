f283c4195d700e5584241af7f6573587349d7c3b
cdxker <fdenzell@gmail.com>
2024-11-10 04:06:06 -0800

    feat(site): make 5 buttons

--- server/main.py
+++ server/main.py
@@ -2,20 +2,23 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-on = True
+on = [ True, True, True, True, True ]
 
 @app.route("/")
 def home():
     return render_template("home.html", on=on);
 
-@app.route("/toggle")
-def toggle():
+@app.route("/toggle/<int:i>")
+def toggle(i: int):
     global on
 
-    on = not on
+    on[i] = not on[i]
     return redirect(url_for("home"))
 
 @app.route("/get")
 def get():
     global on
     return {"active": on}
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
